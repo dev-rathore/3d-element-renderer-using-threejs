0601de7933ecc970886d1a69fa1881d6708001ca
dev-rathore <rathoredvd3001@gmail.com>
2024-06-23 08:59:23 +0530

    added navigation on home page for vehicles page

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
   return (
-    <div>
-      <h1 className="text-3xl text-slate-700 font-bold underline">
+    <div className="min-h-screen flex flex-col">
+      <h1 className="text-center text-4xl text-slate-700 font-bold">
         Home Page
       </h1>
+      <div className="grow flex flex-col justify-center items-center gap-4">
+        <Link to={'/vehicles'} className="text-lg underline">Go To Vehicles Page</Link>
+      </div>
     </div>
   );
 }
