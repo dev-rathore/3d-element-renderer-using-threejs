6e0e34cbd6c6ad1487aa3485e6205ac258475983
dev-rathore <rathoredvd3001@gmail.com>
2024-06-23 08:50:16 +0530

    fix: build

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,9 +1,9 @@
 import HomePage from "./home.page";
-import ThreejsBasicsPage from "./threejs-basics.page";
+// import ThreejsBasicsPage from "./threejs-basics.page";
 import VehiclesPage from "./vehicles.page";
 
 export {
   HomePage,
   VehiclesPage,
-  ThreejsBasicsPage,
+  // ThreejsBasicsPage,
 };
